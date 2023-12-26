381f5bf1e75b432ab3a781b11b93c18bbacc7a40
sultanaranu <sultanaranu16@gmail.com>
2023-12-26 22:37:17 +0600

    Final Project Submitted

--- project.py
+++ project.py
@@ -1,3 +1,13 @@
-a=2
-b=1+a
-print(b) 
+marks=int(input("enter a number:")) 
+if marks>=90 and marks<=95:
+    print("A")
+
+elif marks>=80 and marks<=89:
+    print("A-")
+
+elif marks>=65 and marks<=75:
+    print("A")
+else:
+    print("fail") 
+
+     
